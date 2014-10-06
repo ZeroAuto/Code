3261dd2890746eafa8bda121ab03c61e5aa34ba2
Mike Cooley <mikejames386@gmail.com>
2014-10-06 01:11:47 -0500

    update reverse string.js to take an array as a value and use charAt

--- reverse_string.js
+++ reverse_string.js
@@ -1,10 +1,13 @@
-function rev(array) {
-	ar = array.split("");
-	ra = [];
-	for (var i=ar.length-1; i>=0; i--) {
-		x = ar.pop();
-		ra.push(x);
+function rev(value) {
+	if (value instanceof Array) {
+		var s = value.join("");
+	} else {
+		var s = value
 	}
-	var yarra = ra.join("");
-	console.log(yarra)
+	r = "";
+	for (var i=s.length-1; i>=0; i--) {
+		var c = s.charAt(i)
+		r += c
+	}
+	console.log(r)
 }
