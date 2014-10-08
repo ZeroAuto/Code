6b0128156c13692083fb81d9bfc42560eba13c6c
Mike <mikejames386@gmail.com>
2014-10-08 00:34:37 -0500

    update luhn.js

--- luhn.js
+++ luhn.js
@@ -2,21 +2,26 @@
 // if an integer is entered that is a valid credit card number it returns true
 
 function luhn(int) {
-	// function splitString(p) {
-	// 	p.toString().split("");
-	// }
+	function splitToString(s) {
+		return s.toString().split("");
+	}
 	s1 = 0
 	s2 = 0
 	if (int === parseInt(int)) {
-		a = int.toString().split("").reverse();
-		for (var i = a.length-1; i>=0; i--) {
-			if (i % 2 === 0) {
-				b = parseInt(a[i]);
-				s1 += b
+		a = splitToString(int).reverse();
+		for (i = 0; i < a.length; i++) {
+			if (i % 2 == 0) {
+				b = parseInt(a[i])*2;
+				if (b >= 10) {
+					s1 += b - 9
+				}
+				else {
+					s1 += b
+				}
 			}
 			else {
-				c = parseInt(a[i])*2;
-				s2 += c
+				f = parseInt(a[i]);
+				s2 += f
 			}
 		}
 	}
@@ -24,13 +29,12 @@ function luhn(int) {
 		return false
 	}
 	var x = s1+s2;
-	var y = x.toString().split("");
-	var z = x[x.length-1];
-	// if (0 == parseInt(y[y.length-1])) {
-	// 	return true
-	// }
-	// else {
-	// 	return false
-	// }
-	console.log(z);
+	var y = splitToString(x);
+	var z = y[y.length-1];
+	if (parseInt(z) == 0) {
+		return true
+	}
+	else {
+		return false
+	}
 }
