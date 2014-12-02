1c1f6de4a722144fe6cc271201e11940b0495f71
Michael Cooley <mikejames386@gmail.com>
2014-12-02 12:34:28 -0600

    add convert base function

---
+++ convert_base.rb
@@ -0,0 +1,20 @@
+def convert(base, value)
+	sum = 0
+	new_value = value.to_s.downcase
+	char_array = new_value.split("").reverse
+	char_array.each_with_index do |x, i|
+		if x.ord >= 97 && x.ord <= 122
+			digit = x.ord - 87
+		else
+			digit = x.to_i
+		end
+		sum += (digit * base**i)
+	end
+	puts sum
+end
+
+convert(2, 101)
+convert(3, 111)
+convert(6, 21)
+convert(11, 12)
+
